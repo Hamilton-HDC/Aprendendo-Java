40cbeaed039cabef6de2277bc7ebd3c8c4f4bc1a
Hamilton Cesar <hamiltondc.dc@gmail.com>
2024-05-31 11:31:23 -0300

    ultimo commit MDE

---
+++ Maratona-java/src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao.Aula04;
+
+import org.graalvm.compiler.lir.stackslotalloc.LSStackSlotAllocator_OptionDescriptors;
+import org.graalvm.compiler.serviceprovider.IsolateUtil;
+
+public class Operadores {
+    // + - / *
+    int numero01 = 10;
+    int numero02 = 20;
+    System.out.println(numero01 - numero02);
+
+}
