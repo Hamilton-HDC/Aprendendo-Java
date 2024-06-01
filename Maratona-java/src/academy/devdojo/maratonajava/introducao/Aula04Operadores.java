ac9ee16169899936cb9add32d8b68fd2ab27cae6
Hamilton Cesar <hamiltondc.dc@gmail.com>
2024-05-31 23:46:12 -0300

    Operadores lógicos

--- Maratona-java/src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ Maratona-java/src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -4,8 +4,30 @@ public class Aula04Operadores {
     public static void main(String[]args) {
         // + - / *
         int numero01 = 10;
-        int numero02 = 20;
-        System.out.println(numero01 - numero02);
+        double numero02 = 20;
+        double resultado = numero01 / numero02;
+        System.out.println( resultado );
+
+        // Operadores logicos
+
+        // Operador resto da soma %
+        int resto = 21%7;
+        System.out.println(resto);
+
+        // Operador de igualdede
+
+        // < >... <= >= .... == !=
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 10;
+        boolean isDezDiferentelDez = 10 != 10;
+        System.out.println("isDezMaiorQueVinte" + isDezMenorQueVinte );
+        System.out.println("isDezMenorQueVinte" + isDezMaiorQueVinte );
+
+        System.out.println("isDezIgualvinte" + isDezIgualVinte );
+        System.out.println("isDezIgualDez" + isDezDiferentelDez);
+
+
     }
 
 }
